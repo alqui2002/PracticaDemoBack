6265c9aa4738124be958dd03ae334f038d25c909
alqui2002 <alcarazrodrigo1c@gmail.com>
2024-10-15 16:24:39 -0300

    Hasta aca esta todo menoe el middleware

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/registerDto';
+import { LoginDto } from './dto/loginDto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+    }
+
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) { 
+    return this.authService.login(loginDto);
+    }
+}
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+export class CreateUserDto {
+    name: string;
+    last_name: string;
+    email: string;
+    password: string; 
+}
